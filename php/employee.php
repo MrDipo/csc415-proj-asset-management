8e3aa2fad48663c3f62c079a86d314b90f6af132
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-12-22 13:12:11 +0100

    sql queries for functions modified

--- php/employee.php
+++ php/employee.php
@@ -1,24 +1,25 @@
 <?php
 require "db_connect.php";
 
+# modify so it checks if request has been made previously and if it is available
 function create_request($employee_id, $asset_id, $conn){
-    $curr_date = date("Y-m-d h:i:sa");
-    $query = "INSERT INTO `request`(`employee_id`, `asset_id`, `status`, `date_created`) VALUES ('$employee_id','$asset_id','requested','$curr_date')";
+    $query = "INSERT INTO `request`(`employee_id`, `asset_id`) VALUES ('$employee_id','$asset_id')";
     $result = mysqli_query($conn, $query);
     return $result;
 }
 
-# might neews a join statement here
-function view_made_requests($employee_id, $asset_id, $conn){
-    $query = "SELECT * FROM `request` WHERE 'employee_id' = '$employee_id' AND 'asset_id' = '$asset_id'";
+# returns an assoc array of all requests made by this employee
+function view_made_requests($employee_id, $conn){
+    $query = "SELECT 'request.*', 'asset.*' FROM 'employee' INNER JOIN 'request' ON 'employee.id' = 'request.employee_id' INNER JOIN 'asset' ON 'request.asset_id' = 'asset.id' WHERE 'employee.id' = '$employee_id';";
     $result = mysqli_query($conn, $query);
     $row = mysqli_fetch_assoc($result);
     return $row;
 }
 
-# here too
-function view_assigned_assets($employee_id, $asset_id, $conn){
-    $query = "SELECT * FROM `employee_asset` WHERE 'employee_id' = '$employee_id' AND 'asset_id' = '$asset_id'";
+# returns an assoc array of all assets belonging to this employee
+function view_assigned_assets($employee_id, $conn){
+    $query = "SELECT 'asset.*' FROM 'employee' INNER JOIN 'employee_asset' ON 'employee.id' = 'employee_asset.employee_id' INNER JOIN 'asset' ON 'employee_asset.asset_id' = 'asset.id'
+    WHERE 'employee.id' = '$employee_id';";
     $result = mysqli_query($conn, $query);
     $row = mysqli_fetch_assoc($result);
     return $row;
