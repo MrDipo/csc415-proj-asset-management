ab3349203900a5ef6b6d1a8ee6b7bbd0a74cc861
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-12-22 13:10:30 +0100

    approve request func updated

--- php/manager.php
+++ php/manager.php
@@ -2,15 +2,20 @@
     require "db_connect.php";
     function reject_request($employee_id, $asset_id, $conn){
         $curr_date = date("Y-m-d h:i:sa");
-        $query = "UPDATE `request` SET `status`='rejected',`date_resolved`='$curr_date' WHERE `employee_id`='$employee_id' AND `asset_id`='$asset_id'";
+        $query = "UPDATE `request` SET `status`='rejected',`date_resolved`='$curr_date' WHERE `employee_id`='$employee_id' AND `asset_id`='$asset_id';";
         $result = mysqli_query($conn, $query);
         return $result;
     }
 
+    # set request status to approved and insert into employee_asset
     function approve_request($employee_id, $asset_id, $conn){
         $curr_date = date("Y-m-d h:i:sa");
-        $query = "UPDATE `request` SET `status`='approved',`date_resolved`='$curr_date' WHERE `employee_id`='$employee_id' AND `asset_id`='$asset_id'";
+        $query = "UPDATE `request` SET `status`='approved',`date_resolved`='$curr_date' WHERE `employee_id`='$employee_id' AND `asset_id`='$asset_id';";
+        $asset_query = "INSERT INTO 'employee_asset'('employee_id', 'asset_id') VALUES ('$employee_id', '$asset_id);";
         $result = mysqli_query($conn, $query);
+        if ($result){
+            $effect = mysqli_query($conn, $asset_query);
+        }
         return $result;
     }
 
