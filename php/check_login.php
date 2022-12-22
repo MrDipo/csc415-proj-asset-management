d1bc5af58eea7ed86ed90cdd9de8042e9feaf880
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-12-22 13:10:51 +0100

    che login actually redirects instead

--- php/check_login.php
+++ php/check_login.php
@@ -4,5 +4,6 @@ if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true) {
     echo "Welcome to the member's area, " . htmlspecialchars($_SESSION['username']) . "!";
 } else {
     echo "Please log in first to see this page.";
+    header('Location: ../index.html');
 }
 ?>
