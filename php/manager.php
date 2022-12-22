549e3b7ee70fa17931276aa42d64fe4a07fb00d4
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-12-22 22:31:54 +0100

    deleted some whitespace

--- php/manager.php
+++ php/manager.php
@@ -19,16 +19,4 @@
         }
         return $result;
     }
-
-    # return all requests with their statuses
-    function show_status_requests($conn){
-        $query = "SELECT 'department.name', 'employee.name', 'employee.role', 'request.status', 'request.date_created', 'request.date_resolved', 'asset.name'
-        FROM 'department'
-        INNER JOIN 'employee' ON 'department.id' = 'employee.department_id'
-        INNER JOIN 'request' ON 'employee.id' = 'request.employee_'id'
-        INNER JOIN 'asset' ON 'request.asset_id' = 'asset.id';";
-        $result = mysqli_query($conn, $query);
-        $row = mysqli_fetch_assoc($result);
-        return $row;
-    }
 ?>
