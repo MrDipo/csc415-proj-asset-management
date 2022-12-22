2af0362f12ae1615fc59c912d4643aa8cd551e2b
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-12-22 23:08:30 +0100

    employee CRUD functions implemented

--- php/hr.php
+++ php/hr.php
@@ -1,38 +1,79 @@
 <?php
 require "db_connect.php";
 
-function view_all_requests($conn){
-    $query = "SELECT * FROM `request`";
-    $result = mysqli_query($conn, $query);
-    $row = mysqli_fetch_assoc($result);
-    return $row;
-}
-
 # use join statement here as well
-function view_employee_assets($employee_id, $asset_id, $conn){
-    $query = "SELECT * FROM `employee_asset` WHERE 'employee_id' = '$employee_id'";
+function view_assets_assigned($conn){
+    $query = "SELECT department.name, employee.name, employee.role, asset.name
+    FROM department
+    INNER JOIN employee ON department.id = employee.department_id
+    INNER JOIN employee_asset ON employee.id = employee_asset.employee_id
+    INNER JOIN asset ON employee_asset.asset_id = asset.id;";
     $result = mysqli_query($conn, $query);
     $row = mysqli_fetch_assoc($result);
     return $row;
 }
 
-function create_employee(){
-
+function create_employee($department_id, $name, $address, $phone, $email, $password, $role, $employement_type, $conn){
+    $query = "INSERT INTO 'employee'(`department_id`, `name`, `address`, `phone`, `email`, `password`, `role`, `employment_type`)
+    VALUES ('$department_id','$name','$address','$phone','$email','$password','$role','$employement_type');";
+    $result = mysqli_query($conn, $query);
+    return $result;
 }
 
-function update_employee(){
-    
+function update_employee($employee_id, $department_id, $name, $address, $phone, $email, $password, $role, $employment_type, $conn){
+    $query = "UPDATE employee
+    SET department_id = CASE 
+        WHEN department_id = department_id THEN department_id
+        ELSE '$department_id'
+    END,
+    name = CASE 
+        WHEN name = name THEN name
+        ELSE '$name'
+    END,
+    address = CASE 
+        WHEN address = address THEN address
+        ELSE '$address'
+    END,
+    phone = CASE 
+        WHEN phone = phone THEN phone
+        ELSE '$phone'
+    END,
+    email = CASE 
+        WHEN email = email THEN email
+        ELSE '$email'
+    END,
+    password = CASE 
+        WHEN password = password THEN password
+        ELSE '$password'
+    END,
+    role = CASE 
+        WHEN role = role THEN role
+        ELSE '$role'
+    END,
+    employment_type = CASE 
+        WHEN employment_type = employment_type THEN employment_type
+        ELSE '$employment_type'
+    END,
+    WHERE id = '$employee_id';";
+    $result = mysqli_query($conn, $query);
+    return $result;
 }
 
-function delete_employee(){
-    
+function get_all_employees($conn){
+    $query = "SELECT * from employee;";
+    $result = mysqli_query($conn, $query);
+    $row = mysqli_fetch_assoc($result);
+    return $row;
 }
 
-function get_all_employees(){
-    
+function get_employee($employee_id, $conn){
+    $query = "SELECT * from employee WHERE employee.id = '$employee_id';";
+    $result = mysqli_query($conn, $query);
+    $row = mysqli_fetch_assoc($result);
+    return $row;
 }
 
-function get_employee(){
-    
+function delete_employee(){    
 }
+
 ?>
