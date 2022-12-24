0c864eb59569abff732321f6e0eb4b2e23f6383a
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-12-24 11:39:32 +0100

    not much

--- php/manager.php
+++ php/manager.php
@@ -11,7 +11,7 @@
     function approve_request($employee_id, $asset_id, $conn){
         # $curr_date = date("Y-m-d h:i:sa");
         $query = "UPDATE `request` SET `status`='approved',`date_resolved`= NOW() WHERE `employee_id`='$employee_id' AND `asset_id`='$asset_id';";
-        $asset_insert = "INSERT INTO 'employee_asset'('employee_id', 'asset_id') VALUES ('$employee_id', '$asset_id);";
+        $asset_insert = "INSERT INTO 'employee_asset'('employee_id', 'asset_id') VALUES ('$employee_id', '$asset_id');";
         $result = mysqli_query($conn, $query);
         if ($result){
             $temp = mysqli_query($conn, $asset_insert);
