687f0a980151416c586602f4c01c29c55e731d5c
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-12-24 11:45:04 +0100

    removed the echo statement to check if its working

--- php/check_login.php
+++ php/check_login.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true) {
-    echo "Welcome to the member's area, " . htmlspecialchars($_SESSION['username']) . "!";
 } else {
     echo "Please log in first to see this page.";
     header('Location: ../index.html');
