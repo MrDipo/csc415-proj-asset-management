6f3a4573bc6938dda8a113cf0e42ce0e9786536e
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-12-24 17:53:01 +0100

    converted manager functions to avoid injection

--- php/manager.php
+++ php/manager.php
@@ -1,22 +1,25 @@
 <?php
     require "db_connect.php";
     function reject_request($employee_id, $asset_id, $conn){
-        # $curr_date = date("Y-m-d h:i:sa");
-        $query = "UPDATE `request` SET `status`='rejected',`date_resolved`= NOW() WHERE `employee_id`='$employee_id' AND `asset_id`='$asset_id';";
-        $result = mysqli_query($conn, $query);
-        return $result;
+        $query = "UPDATE `request` SET `status`='rejected',`date_resolved`= NOW() WHERE `employee_id`=? AND `asset_id`=?;";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("ii", $employee_id, $asset_id);
+        $stmt->execute();
+        return $stmt->get_result();
     }
 
     # set request status to approved and insert into employee_asset
     function approve_request($employee_id, $asset_id, $conn){
-        # $curr_date = date("Y-m-d h:i:sa");
-        $query = "UPDATE `request` SET `status`='approved',`date_resolved`= NOW() WHERE `employee_id`='$employee_id' AND `asset_id`='$asset_id';";
-        $asset_insert = "INSERT INTO 'employee_asset'('employee_id', 'asset_id') VALUES ('$employee_id', '$asset_id');";
-        $result = mysqli_query($conn, $query);
-        if ($result){
-            $temp = mysqli_query($conn, $asset_insert);
-            unset($temp);
+        $query = "UPDATE `request` SET `status`='approved',`date_resolved`= NOW() WHERE `employee_id`=? AND `asset_id`=?;";
+        $asset_insert = "INSERT INTO 'employee_asset'('employee_id', 'asset_id') VALUES (?, ?);";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("ii", $employee_id, $asset_id);
+        $stmt->execute();
+        if ($stmt->get_result()){
+            $secondstmt = $conn->prepare($asset_insert);
+            $secondstmt->bind_param("ii", $employee_id, $asset_id);
+            $stmt->execute();
         }
-        return $result;
+        return $stmt->get_result();
     }
 ?>
